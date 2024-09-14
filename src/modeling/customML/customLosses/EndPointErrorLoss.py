a6c411ceff0f8a0b16eab617a5d6c078c00f6bf4
bkr11a <bkr11a@hotmail.com>
2024-09-14 22:18:57 +0800

    Split custom losses into seperate manageable files

---
+++ src/modeling/customML/customLosses/EndPointErrorLoss.py
@@ -0,0 +1,20 @@
+__author__ = "Brad Rice"
+__version__ = 0.1
+
+import tensorflow as tf
+
+class EPE_Loss(tf.keras.losses.Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    @tf.function
+    def call(self, y_true, y_pred):
+        u_true = y_true[:, :, :, 0]
+        u_pred = y_pred[:, :, :, 0]
+
+        v_true = y_true[:, :, :, 1]
+        v_pred = y_pred[:, :, :, 1]
+
+        epe = tf.reduce_mean(tf.sqrt(tf.square(u_true - u_pred) + tf.square(v_true - v_pred)))
+
+        return epe
