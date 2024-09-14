c4a2bebf5cf2b8613154eae17a608f0ac4e9ad82
bkr11a <bkr11a@hotmail.com>
2024-09-14 22:20:55 +0800

    Refactor end point error class name to average end point error

--- src/modeling/customML/customLosses/EndPointErrorLoss.py
+++ src/modeling/customML/customLosses/EndPointErrorLoss.py
@@ -3,7 +3,7 @@ __version__ = 0.1
 
 import tensorflow as tf
 
-class EPE_Loss(tf.keras.losses.Loss):
+class AEPE_Loss(tf.keras.losses.Loss):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     
